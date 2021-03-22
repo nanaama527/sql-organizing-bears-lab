8a4a314471d50d64a661b366e6faef3a46603566
Nana <72888369+nanaama527@users.noreply.github.com>
2021-03-21 22:11:34 -0400

    commit tree 815fadfb0791f82cfecb9a5610b057ae8f89029e

--- lib/create.sql
+++ lib/create.sql
@@ -2,8 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender TEXT,
+  sex TEXT,
   color	TEXT,
   temperament TEXT,
-  alive	BOOLEAN,
-);
+  alive	BOOLEAN);
